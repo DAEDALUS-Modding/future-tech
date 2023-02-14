20a3188656b3422eb946a95a559969a3b5709b39
Ferret-Master <64487611+Ferret-Master@users.noreply.github.com>
2023-02-14 13:25:02 -0500

    Initial commit
    Co-Authored-By: Tony Taiga <67127659+Cyber-Punk-Panda@users.noreply.github.com>
    Co-Authored-By: billyblueboy <52869346+billyblueboy@users.noreply.github.com>

---
+++ ui/mods/future_tech/icon_atlas.js
@@ -0,0 +1,16 @@
+  
+var paeiouIcons = [
+	"ft_aa_walker",
+	"ft_assault_walker",
+	"ft_commander",
+	"ft_fabricator",
+	"ft_factory",
+	"ft_grinder_mex",
+	"ft_rocket_tank",
+	"ft_sniper_tank",
+	"ft_solar_generator",
+	"ft_standard_tank",
+
+	
+];
+model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
---
+++ ui/mods/future_tech/shared_build.js
@@ -0,0 +1,20 @@
+var newBuildScenario = {
+
+
+    "/pa/units/land/ft_aa_walker/ft_aa_walker.json": ["ft_land_units", 1, {row: 2, column: 1}],
+    "/pa/units/land/ft_assault_walker/ft_assault_walker.json": ["ft_land_units", 1, {row: 2, column: 2}],
+    "/pa/units/land/ft_fabricator/ft_fabricator.json": ["ft_land_units", 1, {row: 2, column: 0}],
+    "/pa/units/land/ft_standard_tank/ft_standard_tank.json": ["ft_land_units", 1, {row: 2, column: 3}],
+    "/pa/units/land/ft_sniper_tank/ft_sniper_tank.json": ["ft_land_units", 1, {row: 2, column: 4}],
+    "/pa/units/land/ft_rocket_tank/ft_rocket_tank.json": ["ft_land", 1, {row: 1, column: 0}],
+
+    "/pa/units/land/ft_factory/ft_factory.json": ["ft_land", 1, {row: 2, column: 0}],
+    "/pa/units/land/ft_grinder_mex/ft_grinder_mex.json": ["ft_land", 1, {row: 2, column: 1}],
+    "/pa/units/land/ft_solar_generator/ft_solar_generator.json": ["ft_land", 1, {row: 2, column: 2}],
+    
+
+  
+}
+if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
+    _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuildScenario);
+}
