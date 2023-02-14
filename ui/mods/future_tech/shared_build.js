e2fb4c314bdb969e26b3a3758cb927384d3ad6d6
quildtide <42811940+quildtide@users.noreply.github.com>
2023-02-14 14:02:38 -0500

    Revert "Fix ft_rocket_tank build bar tab"
    This reverts commit a091cb23dadee56ac9b0e910447dabbc4fb69434.

--- ui/mods/future_tech/shared_build.js
+++ ui/mods/future_tech/shared_build.js
@@ -6,7 +6,7 @@ var newBuildScenario = {
     "/pa/units/land/ft_fabricator/ft_fabricator.json": ["ft_land_units", 1, {row: 2, column: 0}],
     "/pa/units/land/ft_standard_tank/ft_standard_tank.json": ["ft_land_units", 1, {row: 2, column: 3}],
     "/pa/units/land/ft_sniper_tank/ft_sniper_tank.json": ["ft_land_units", 1, {row: 2, column: 4}],
-    "/pa/units/land/ft_rocket_tank/ft_rocket_tank.json": ["ft_land_units", 1, {row: 1, column: 0}],
+    "/pa/units/land/ft_rocket_tank/ft_rocket_tank.json": ["ft_land", 1, {row: 1, column: 0}],
 
     "/pa/units/land/ft_factory/ft_factory.json": ["ft_land", 1, {row: 2, column: 0}],
     "/pa/units/land/ft_grinder_mex/ft_grinder_mex.json": ["ft_land", 1, {row: 2, column: 1}],
